dd40914aa607e06bd0daad37f65c9d7f40bd1eb7
nielsonmuniz <nielsonhc@outlook.com>
2020-10-30 14:45:46 -0300

    Start Commit

--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -10,27 +10,66 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
 
   ContactHelper helper = ContactHelper();
+  List<Contact> contacts = List();
 
 
   @override
   void initState() {
     super.initState();
 
-    Contact c = Contact();
-    c.name = "aosdkjaskjd";
-    c.email = "asdasdasd";
-    c.phone = "12312391823";
-    c.img = "imgtest";
-
-    helper.saveContact(c);
-
     helper.getAllContacts().then((list){
-      print(list);
+      setState(() {
+        contacts = list;
+      });
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text ("Contatos"),
+        backgroundColor: Colors.red,
+        centerTitle: true,
+      ),
+      backgroundColor: Colors.white,
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){},
+        child: Icon(Icons.add),
+        backgroundColor: Colors.red,
+      ),
+      body: ListView.builder(
+          padding: EdgeInsets.all(10.0),
+          itemCount: contacts.length,
+          itemBuilder: (context, index) {
+
+          },
+
+      ),
+    );
+  }
+
+  Widget _contactCard(BuildContext context, int index){
+    return GestureDetector(
+      child: Card(
+        child: Padding(
+          padding: EdgeInsets.all(10.0),
+          child: Row(
+            children: <Widget>[
+              Container(
+                width: 80.0,
+                height: 80.0,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  image: DecorationImage(
+                    image: null
+                  )
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
